a6a853bd1c0331a208e9b6bad2b8e91c7e42c71f
HyeonGeunY <hgyoon0928@gmail.com>
2022-05-02 23:14:18 +0900

    msno

--- notes/tabular_data/data.py
+++ notes/tabular_data/data.py
@@ -1,15 +1,15 @@
-import missingno
+import missingno as msno
 import pandas as pd
 
 
 def visualize_features(df: pd.DataFrame, start_idx: int = 0, end_idx: int = 0):
-    
+
     if end_idx:
         msno.matrix(df=df.iloc[:, start_idx:end_idx], figsize=(20, 14), color=(0.42, 0.1, 0.05))
     else:
         msno.matrix(df=df.iloc[:, start_idx], figsize=(20, 14), color=(0.42, 0.1, 0.05))
-        
-        
+
+
 def checking_percent_of_null(df: pd.DataFrame, stage: str = "train"):
     print(f"Percent of NaN value for {stage}")
     for col in df.columns:
@@ -18,44 +18,51 @@ def checking_percent_of_null(df: pd.DataFrame, stage: str = "train"):
         )
 
 
+# meta data
+
+
 def get_meta_data(df: pd.DataFrame):
     data = []
     for f in df.columns:
         # Defining the role
-        if f == 'Transported':
-            role = 'target'
+        if f == "Transported":
+            role = "target"
         else:
-            role = 'input'
-         
+            role = "input"
+
         # Defining the level
         if f in ["PassengerId", "HomePlanet", "Cabin", "Destination", "Name"]:
-            level = 'nominal'
+            level = "nominal"
         elif f in ["CryoSleep", "VIP"]:
-            level = 'binary'
+            level = "binary"
         elif f in ["RoomService", "FoodCourt", "ShoppingMall", "Spa", "VRDeck"]:
-            level = 'comtinous'
+            level = "comtinous"
         elif f in ["Age"]:
-            level = 'discrete'
+            level = "discrete"
 
         # Initialize keep to True for all variables except for id
         keep = True
-        if f in ['PassengerId', "Name"]:
+        if f in ["PassengerId", "Name"]:
             keep = False
 
-        # Defining the data type 
+        # Defining the data type
         dtype = df[f].dtype
 
         # Creating a Dict that contains all the metadata for the variable
-        f_dict = {
-            'varname': f,
-            'role': role,
-            'level': level,
-            'keep': keep,
-            'dtype': dtype
-        }
+        f_dict = {"varname": f, "role": role, "level": level, "keep": keep, "dtype": dtype}
         data.append(f_dict)
-        
-    
-    meta = pd.DataFrame(data, columns=['varname', 'role', 'level', 'keep', 'dtype'])
-    meta.set_index('varname', inplace=True)
+
+    meta = pd.DataFrame(data, columns=["varname", "role", "level", "keep", "dtype"])
+    meta.set_index("varname", inplace=True)
+    return meta
+
+
+def add_row_to_metadata(meta: pd.DataFrame, info_list: list, index: str):
+    meta = pd.concat(
+        [
+            meta,
+            pd.DataFrame([info_list], columns=["role", "level", "keep", "dtype"], index=[index]),
+        ],
+        axis=0,
+    )
     return meta
